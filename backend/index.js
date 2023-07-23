584ffebe22d67cf09e6f5c10b9eaa2efd95f8a74
Aditya Kitukale <126107933+Adityakitukale499@users.noreply.github.com>
2023-07-23 23:03:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const server = express()
 
 //server.use(cors());
 server.use((req, res, next)=>{
+    console.log("myRespons" ,res);
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next()
