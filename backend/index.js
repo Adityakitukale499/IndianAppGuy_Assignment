fdb4a53761030f4cfa88cf17dcd94da6b8100327
gargvive18 <36004200+gargvive18@users.noreply.github.com>
2023-07-25 20:17:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,9 @@ const server = express()
 //server.use(cors());
 server.use((req, res, next)=>{
     console.log("myRespons" ,res);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   
     next()
 })
